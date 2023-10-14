80a63e7995f6592f361e2396df00a7035a674098
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-10-14 23:37:59 +0530

    utils file added.

---
+++ src/utils/chartDataExtractor.js
@@ -0,0 +1,79 @@
+/* eslint-disable no-inner-declarations */
+import fetchData from "./fetchData.js";
+
+async function processAndExportData() {
+	try {
+		const apiData = await fetchData();
+
+		const categories = {};
+
+		const barGraph = {
+			male: {},
+			female: {},
+		};
+
+		function sortdata(data) {
+			data.forEach((item) => {
+				if (item.nat in categories) {
+					categories[item.nat] = categories[item.nat] + 1;
+				} else {
+					categories[item.nat] = 1;
+				}
+			});
+		}
+
+		function sortBarGraphData(data) {
+			data.forEach((item) => {
+				if (item.gender === "male") {
+					if (item.nat in barGraph.male) {
+						barGraph.male[item.nat] = barGraph.male[item.nat] + 1;
+					} else {
+						barGraph.male[item.nat] = 1;
+					}
+				}
+				if (item.gender === "female") {
+					if (item.nat in barGraph.female) {
+						barGraph.female[item.nat] = barGraph.female[item.nat] + 1;
+					} else {
+						barGraph.female[item.nat] = 1;
+					}
+				}
+			});
+		}
+
+		sortdata(apiData.results);
+
+		sortBarGraphData(apiData.results);
+
+		const keys = Object.keys(barGraph.male);
+
+		keys.sort();
+
+		const sortedBarGraph = {
+			male: {},
+			female: {},
+		};
+
+		for (const key of keys) {
+			sortedBarGraph.male[key] = barGraph.male[key];
+		}
+
+		const keys2 = Object.keys(barGraph.female);
+
+		keys2.sort();
+
+		for (const key of keys2) {
+			sortedBarGraph.female[key] = barGraph.female[key];
+		}
+
+		return {
+			categories,
+			sortedBarGraph,
+		};
+	} catch (error) {
+		console.error("Error processing data:", error);
+		throw error;
+	}
+}
+
+export default processAndExportData;
---
+++ src/utils/constants.js
@@ -0,0 +1,39 @@
+import revenues from "../assets/revenues.png"
+import transactions from "../assets/transactions.png"
+import likes from "../assets/like.png"
+import people from "../assets/people.png"
+
+export const  topCardData = [
+  {
+    image: revenues,
+    title: "Total Revenues",
+    price: "$2,129,430",
+    percent: "+2.5%",
+    color: "#7FCD93",
+  },
+
+  {
+    image: transactions,
+    title: "Total Transactions",
+    price: "1,520",
+    percent: "+1.7%",
+    color: "#7FCD93",
+  },
+
+  {
+    image: likes,
+    title: "Total Likes",
+    price: "9,721",
+    percent: "+1.4%",
+    color: "red",
+  },
+
+  {
+    image: people,
+    title: "Total Users",
+    price: "9,721",
+    percent: "+4.2%",
+    color: "#A9B0E5",
+  },
+]
+
