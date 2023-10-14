4fbba1e0d1fab27e6cac375fa20a7e798562661b
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-10-15 00:23:45 +0530

    add jsx file added

---
+++ src/components/Add.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Add = () => {
+  return (
+    <div className='w-[510px] flex flex-col justify-center items-center border-2 h-[280px] mt-[15px] shadow-xl rounded-[20px]'>
+      <button type='button' className='text-[grey] flex flex-row justify-center items-center w-[100px] h-[100px] bg-[#eaeaea] rounded-full font-[200] text-[79px] text-center'><span className='mt-[-15px]'>+</span></button>
+
+      <div className='text-[16.5px] font-[500] text-[grey]'>App Profile</div>
+    </div>
+  )
+}
+
+export default Add
