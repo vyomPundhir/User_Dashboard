0ee071f8f5888d733bbc353cfc67cc548eeb051f
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-10-15 00:45:26 +0530

    Signin page modified

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -21,13 +21,13 @@ const SignIn = () => {
 
             <div className='flex flex-row items-center gap-[40px] mt-[364px] ml-[78px]'>
 
-              <img src="src\assets\github.png" alt="github" className='w-[46px] h-[46px]'/>
+              <img src="src\assets\github.png" alt="github" className='w-[46px] h-[46px] cursor-pointer'/>
 
-              <img src="src\assets\twitter.png" alt="twitter" className='w-[45px] h-[45px] rounded-full' />
+              <img src="src\assets\twitter.png" alt="twitter" className='w-[45px] h-[45px] rounded-full cursor-pointer' />
 
-              <img src="src\assets\linkedin.png" alt="linkedIn" className='w-[56px] h-[56px]' />
+              <img src="src\assets\linkedin.png" alt="linkedIn" className='w-[56px] h-[56px] cursor-pointer' />
 
-              <img src="src\assets\discord.png" alt="discord" className='w-[50px] h-[50px]' />
+              <img src="src\assets\discord.png" alt="discord" className='w-[50px] h-[50px] cursor-pointer' />
 
             </div>
 
@@ -46,38 +46,38 @@ const SignIn = () => {
 
           <div className='flex flex-row items-center gap-[30px]'>
 
-            <div className='h-[30px] w-[210px] flex flex-row justify-center items-center gap-[15px] bg-[#ffffff] rounded-[20px]'>
+            <button className='h-[30px] w-[210px] flex flex-row justify-center items-center gap-[15px] bg-[#ffffff] rounded-[20px] shadow-xl'>
               <img src="src\assets\google.png" alt="google" className='w-[18px] h-[18px]'/>
               <span className='text-[#a1a1a1] text-[14px] font-[500]'>Sign in with Google</span>
-            </div>
+            </button>
 
-            <div className='h-[30px] w-[200px] flex flex-row justify-center items-center gap-[15px] bg-[#ffffff] rounded-[20px]'>
+            <button className='h-[30px] w-[200px] flex flex-row justify-center items-center gap-[15px] bg-[#ffffff] rounded-[20px] shadow-xl'>
               <img src="src\assets\apple.png" alt="apple" className='w-[17px] h-[17px]' />
               <span className='text-[#a1a1a1] text-[14px] font-[500]'>Sign in with Apple</span>
-            </div>
+            </button>
 
           </div>
           
-          <form action="" className='flex flex-col gap-[20px] w-[440px] h-[365px] bg-[white] rounded-[18px]'>
+          <form action="" className='flex flex-col gap-[20px] w-[440px] h-[365px] bg-[white] rounded-[18px] shadow-xl'>
 
             <div className='flex flex-col ml-[30px] mr-[30px] mt-[30px] gap-[9px]'>
               <label className='font-[500] text-[17.5px]'>Email address</label>
-              <input type="text" name="email" id="email" placeholder='Enter email address' className='rounded-[10px] h-[45px] bg-[#EAEAEA] text-[17px] pl-[20px]'/>
+              <input type="text" name="email" id="email" placeholder='Enter email address' className='rounded-[10px] h-[45px] bg-[#EAEAEA] text-[17px] pl-[20px] focus:outline-none'/>
             </div>
 
             <div className='flex flex-col ml-[30px] mr-[30px] gap-[9px]'>
               <label className='font-[500] text-[17.5px]'>Password</label>
-              <input type="password" name="password" id="password" placeholder='Enter password' className='rounded-[10px] bg-[#EAEAEA] text-[17px] h-[45px] pl-[20px]'/>
+              <input type="password" name="password" id="password" placeholder='Enter password' className='rounded-[10px] bg-[#EAEAEA] text-[17px] h-[45px] pl-[20px] focus:outline-none'/>
             </div>
 
-            <div className='ml-[30px] mr-[30px] text-[17px] font-[500] text-[#4083F3]'>Forgot password?</div>
+            <button className='ml-[30px] mr-[30px] text-[17px] font-[500] text-[#4083F3] text-left'>Forgot password?</button>
 
             <button type="submit" className='rounded-[10px] text-[17px] font-[600] tracking-[1px] h-[45px] bg-[#4083F3] text-[white] ml-[30px] mr-[30px] flex flex-row justify-center items-center w-[380px]'>Sign In</button>
 
           </form>
 
           <div className='text-[17px] font-[600] text-[#a0a0a0] text-center'>
-            Don't have an account? <span className='text-[#4083F3] font-[600]'>Register here</span>
+            Don't have an account? <button className='text-[#4083F3] font-[600]'>Register here</button>
           </div>
 
         </div>
